c0202204e9e18ffb2b272db1bb68ba728f0b5a58
sadikcoban <coban.sadik@icloud.com>
2022-07-31 10:47:07 +0300

    first commit

---
+++ iOS-FirebaseChat-NoStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-FirebaseChat-NoStoryboard
+//
+//  Created by Sadık Çoban on 31.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
